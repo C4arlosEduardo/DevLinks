5ed1c142bfdbd7adf5632256424aaf264ce13ec7
Carlos Eduardo <kadu@MacBook-Air-de-Carlos.local>
2025-04-06 15:45:56 -0300

    :tada

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode(){
+    const html = document.documentElement
+// alterna a classe
+  if(html.classList.contains('light')){
+     html.classList.remove('light')
+ } else {
+     html.classList.add('light')
+   }
+    //html.classsList.toggle('light') - este código não funcionou.
+
+    // pegar a tag img
+      const img = document.querySelector('#profile img')
+
+      // substituir a imagem e descrição
+      if(html.classList.contains('light')){
+       // se tiver ligth mode, adcionar a imagem light
+       img.setAttribute('src', '/assets/avl/avatarlight.png')
+       img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos escuros e blusa de couro preto e fundo roxo')
+      } else {
+
+   // se tiver sem ligth mode, manter a imagem normal
+   img.setAttribute('src', '/assets/avatar.png')
+   img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e blusa de couro preto e fundo roxo')
+}
+
+
+}
